cfd0c1d0422279249b221270b62efab2903da127
fuxuan <1037074516@qq.com>
2020-03-29 11:51:56 +0800

    project commit

---
+++ react/src/ListItem.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+import './ListItem.css'
+const ListItem = (props) =>{
+   const item = props.item;
+
+   if (item.done){
+       return <p className="done-item">{item.content}</p>
+   }else{
+       return <p className="item">{item.content}</p>
+   }
+}
+
+
+
+
+/*
+class ListItem extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (<p>(this.props.item)</p>)
+
+    };
+}*/
+export default ListItem;
